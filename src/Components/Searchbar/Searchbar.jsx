5309c30a23eb82eeceb19abce1c5b439554ba3a8
uncle-Stas <stanislav.zavadskyi11@gmail.com>
2022-10-04 14:42:55 +0300

    break

--- src/Components/Searchbar/Searchbar.jsx
+++ src/Components/Searchbar/Searchbar.jsx
@@ -1,6 +1,8 @@
 import css from './Searchbar.module.css';
 
 import { Component } from 'react';
+import { GoSearch } from 'react-icons/go';
+import { IconContext } from 'react-icons';
 
 class Searchbar extends Component {
   render() {
@@ -10,6 +12,9 @@ class Searchbar extends Component {
           <form className={css.form}>
             <button type="submit" className={css.button}>
               {/* <span className={css.buttonLabel}>Search</span> */}
+              <IconContext.Provider value={{ className: `${css.icon}` }}>
+                <GoSearch />
+              </IconContext.Provider>
             </button>
 
             <input
